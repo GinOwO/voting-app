b9d5bdcfd4b08ef969d34c7f53e05c0b066016c9
GinOwO <65128789+GinOwO@users.noreply.github.com>
2024-04-15 20:23:13 +0530

    fix

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,6 +4,10 @@ export default function Intro(props) {
     return (
         <div className="intro-container">
             <p className="intro">Click the button below to start voting</p>
+            <p>
+                Source code at{" "}
+                <a href="https://github.com/GinOwO/voting-app">here</a>
+            </p>
             <button className="start" onClick={() => props.onStart()}>
                 Start
             </button>
