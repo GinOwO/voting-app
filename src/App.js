71323ca18c829d243de30bacf70d31e5aa8ea958
GinOwO <65128789+GinOwO@users.noreply.github.com>
2024-04-15 20:16:33 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
+import Choice from "./components/Choice";
+import Results from "./components/Results";
+import Intro from "./components/Intro";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const data = [
+        {
+            question: "What is your favorite programming language?",
+            choiceA: "Python",
+            choiceB: "JavaScript",
+        },
+        {
+            question: "What is your favorite front-end framework?",
+            choiceA: "React",
+            choiceB: "Vue",
+        },
+        {
+            question: "What is your favorite back-end framework?",
+            choiceA: "Express",
+            choiceB: "Django",
+        },
+        {
+            question: "What is your favorite database?",
+            choiceA: "MongoDB",
+            choiceB: "PostgreSQL",
+        },
+        {
+            question: "What is your favorite cloud platform?",
+            choiceA: "AWS",
+            choiceB: "GCP",
+        },
+    ];
+
+    const [state, setState] = useState(0);
+    const [question, setQuestion] = useState(0);
+    const [choiceA, setChoiceA] = useState(0);
+    const [choiceB, setChoiceB] = useState(0);
+    const [result, setResult] = useState([]);
+
+    const nextQuestion = (choice) => {
+        if (choice !== -1) {
+            setResult([...result, choice]);
+        }
+        if (question < data.length - 1) {
+            setQuestion(question + 1);
+            setChoiceA(data[question + 1].choiceA);
+            setChoiceB(data[question + 1].choiceB);
+        } else {
+            setState(2);
+        }
+    };
+
+    const restart = () => {
+        setState(0);
+        setQuestion(-1);
+        setResult([]);
+    };
+
+    document.addEventListener("DOMContentLoaded", () => restart());
+
+    const start = () => {
+        setState(1);
+        nextQuestion(-1);
+    };
+
+    return (
+        <>
+            {state === 0 && <Intro onStart={start} />}
+            {state === 1 && (
+                <Choice
+                    question={data[question].question}
+                    choiceA={choiceA}
+                    choiceB={choiceB}
+                    onChoice={nextQuestion}
+                />
+            )}
+            {state === 2 && <Results onRestart={restart} results={result} />}
+        </>
+    );
 }
 
 export default App;
